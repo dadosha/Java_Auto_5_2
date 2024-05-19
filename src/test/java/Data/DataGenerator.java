b6898ca1b0bb0a5abea913a6a86001839b02042a
Станислав Койфман <stanislavkoyfman@gmail.com>
2024-05-19 23:21:47 +0300

    Add tests

--- src/test/java/Data/DataGenerator.java
+++ src/test/java/Data/DataGenerator.java
@@ -1,4 +1,82 @@
 package Data;
 
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
 public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(UserInfo user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+
+    }
+
+    public static String generateName() {
+        return faker.name().firstName();
+    }
+
+    public static String generatePassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String status) {
+            return new UserInfo(generateName(), generatePassword(), status);
+        }
+
+        public static UserInfo changeUserInfo(String name, String password, String status) {
+            return new UserInfo(name, password, status);
+        }
+
+        public static UserInfo getRegisteredUser(String status) {
+            UserInfo user = generateUser(status);
+            sendRequest(user);
+            return user;
+        }
+
+        public static void updateUserStatus(UserInfo user, String status) {
+            UserInfo updateUser = changeUserInfo(user.getLogin(), user.getPassword(), status);
+            sendRequest(updateUser);
+        }
+
+        public static void updateUserPassword(UserInfo user, String password) {
+            UserInfo updateUser = changeUserInfo(user.getLogin(), password, user.getStatus());
+            sendRequest(updateUser);
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String login;
+        String password;
+        String status;
+    }
 }
--- src/test/java/Test/AuthTest.java
+++ src/test/java/Test/AuthTest.java
@@ -1,4 +1,133 @@
 package Test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static Data.DataGenerator.Registration.*;
+import static Data.DataGenerator.generateName;
+import static Data.DataGenerator.generatePassword;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class AuthTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("h2").shouldHave(exactText("Личный кабинет"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = generateUser("active");
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(visible)
+                .shouldHave(exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(visible)
+                .shouldHave(exactText("Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = generateName();
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(visible)
+                .shouldHave(exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = generatePassword();
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(visible)
+                .shouldHave(exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should login if change password")
+    void changePasswordTest() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("h2").shouldHave(exactText("Личный кабинет"));
+        open("http://localhost:9999");
+
+        var newPassword = generatePassword();
+        updateUserPassword(registeredUser, newPassword);
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(newPassword);
+        $("[data-test-id='action-login']").click();
+        $("h2").shouldHave(exactText("Личный кабинет"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login after change status on blocked")
+    void changeStatusToBlockedStatus() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("h2").shouldHave(exactText("Личный кабинет"));
+        open("http://localhost:9999");
+
+        updateUserStatus(registeredUser, "blocked");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(visible)
+                .shouldHave(exactText("Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    @DisplayName("Should login after change status on active")
+    void changeStatusToActiveStatus() {
+        var registeredUser = getRegisteredUser("blocked");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(visible)
+                .shouldHave(exactText("Ошибка! Пользователь заблокирован"));
+
+        updateUserStatus(registeredUser, "active");
+        $("[data-test-id='action-login']").click();
+        $("h2").shouldHave(exactText("Личный кабинет"));
+    }
 }
